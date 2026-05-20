7fca00e9d3b4b69a0b4776009dec14e3fb31246e
jason-heroes620 <jason.w@heroes.my>
2026-05-20 15:12:56 +0800

    removed logging

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -169,7 +169,6 @@ class PaymentController extends Controller
 
     public function payment(Request $request, string $refNo)
     {
-        Log::info('ref no', $refNo);
         $order = Orders::query()->where('order_no', $refNo)->first();
         Log::info($order);
         $invoice = Invoices::query()->where('order_id', $order->order_id)->first();
