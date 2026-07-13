1f0bc95a6e39a0b501044593eb8350d309d5b75b
jason-heroes620 <jason.w@heroes.my>
2026-07-13 14:47:31 +0800

    update pdf vendor config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -21,4 +21,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->booting(function () {
+        // Force register the DomPDF service provider if autodiscovery failed
+        if (class_exists(\Barryvdh\DomPDF\ServiceProvider::class)) {
+            app()->register(\Barryvdh\DomPDF\ServiceProvider::class);
+        }
+    })
+    ->create();
